01626d5c77b2ddfb3717c61a8f6fe204109378c8
rcampion <campion.richard@gmail.com>
2019-06-17 00:52:34 -0400

    Cleanup

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,3 +1,2 @@
-// export const webServiceEndpoint = 'https://www.zdslogic.com/ng4-contacts-server/api';
-export const webServiceEndpoint = 'http://localhost:8080/ng4-contacts-server/api';
+
 export let defaultItemsCountPerPage = 10;
